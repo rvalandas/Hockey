050b631e286d084fb05cb2ed9980d213a2d3690a
rvalandas <rvalandas@gmail.com>
2016-10-13 13:24:37 -0700

    Update class.py

--- class.py
+++ class.py
@@ -15,3 +15,23 @@ emp2 = Player("Mani", 3000)
 emp1.displayPlayer()
 emp2.displayPlayer()
 print "Total players %d" % Player.playerCount
+
+
+Class Bench:
+	benchCount = 0
+	def __init__(self, name, number)
+		self.name = name
+		self.number = number
+		Bench.benchCount +=1
+	def displayBench(self)
+		print "Bench name = ", self.name, "Bench number = ", self.number
+bench1 = Bench("Ashwin", 1)
+bench2 = Bench("Mishra", 2)
+
+bench1.displayBench()
+bench2.displayBench()
+
+
+
+
+		
